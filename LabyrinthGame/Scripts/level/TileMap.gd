03a4c0400d543891e39f9a17f03ef30e4c4d4caa
Christopher Price <cspprice@gmail.com>
2019-04-23 13:53:09 -0700

    added comments to the player script

--- LabyrinthGame/Scripts/level/TileMap.gd
+++ LabyrinthGame/Scripts/level/TileMap.gd
@@ -24,8 +24,3 @@ func _ready():
 				set_cellv(gridSet[i], 0)
 	
 	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
--- LabyrinthGame/Scripts/player/player.gd
+++ LabyrinthGame/Scripts/player/player.gd
@@ -3,41 +3,51 @@ extends Sprite
 onready var ray = $RayCast2D
 
 var speed = 256
-var tile_size = 64
+var tile_size = 64 
 
 var last_position = self.position
 var target_position = self.position
 var movedir = Vector2(0,0)
 
 func _ready():
+	# Snaps player to the grid
 	position = target_position.snapped(Vector2(tile_size, tile_size))
 	last_position = position 
 	target_position = position
 
 func get_movedir():
+	# Define directions player can move
 	var LEFT = Input.is_action_pressed("ui_left")
 	var RIGHT = Input.is_action_pressed("ui_right")
 	var UP = Input.is_action_pressed("ui_up")
 	var DOWN = Input.is_action_pressed("ui_down")
 	
+	# prevents the player from trying to move in multiple directions
 	movedir.x = -int(LEFT) + int(RIGHT)
 	movedir.y = -int(UP) + int(DOWN)
 	
+	# prevents the player from moving diagonally
 	if(movedir.x != 0 && movedir.y != 0):
 		movedir = Vector2(0, 0)
+		
+	#
 	if(movedir != Vector2(0, 0)):
 		ray.cast_to = movedir * tile_size /2
 
 
 func _process(delta):
+	# records the current positon of the player when idle and checks what direction the player wants to move
 	if (position == target_position):
 		get_movedir()
 		last_position = position
 		target_position += movedir * tile_size
 		
 	else:
+		#if the player wants to move in a direction that has a wall, sets the position back to its last position
 		if(ray.is_colliding()):
 			target_position = last_position
+			
+		# otherwise move the player to the new location 
 		else:
 			position += speed * movedir * delta
 			var distance = (position - last_position).abs().length()
